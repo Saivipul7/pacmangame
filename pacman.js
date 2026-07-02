fa9f13ebc57a59da177d393424ec609f300ebb91
Saivipul7 <saivipul147@gmail.com>
2026-07-02 10:29:55 +0530

    Updated pacman images

--- pacman.js
+++ pacman.js
@@ -8,6 +8,13 @@ const boardHeight = rowCount * tileSize;
 let context;
 
 //sprites
+let blueGhost;
+let orangeGhost;
+let pinkGhost;
+let redGhost;
+let pacman;
+let wall;
+
 
 window.onload = function(){
     board = document.getElementById("board");
@@ -19,3 +26,43 @@ window.onload = function(){
 //provides methods and properties for drawing and manipulating graphics on the canvas.
 
 
+function loadImages() {
+    //wall image 
+    wall = new Image();
+    wall.src = "png/wall.png";
+
+
+    //ghost images
+
+    blueGhost = new Image();
+    blueGhost.src = "png/blueghost.png";
+
+    orangeGhost = new Image();
+    orangeGhost.src = "png/orangeghost.png";
+
+    pinkGhost = new Image();
+    pinkGhost.src = "png/pinkghost.png";
+
+    redGhost = new Image();
+    redGhost.src = "png/redghost.png";
+
+    //pacman images
+
+    pacmanup = new Image();
+    pacmanup= "png/pacmanup.png";
+    
+    pacmandown = new Image();
+    pacmandown= "png/pacmandown.png";
+
+    
+    pacmanleft = new Image();
+    pacmanleft= "png/pacmanleft.png";
+
+    
+    pacmanright = new Image();
+    pacmanright= "png/pacmanright.png";
+
+    
+
+
+}
