50a32cbf4a38c5bd30eecb3b5e8f11a3739336af
Saivipul7 <saivipul147@gmail.com>
2026-07-02 10:06:55 +0530

    Initial commit

--- pacman.js
+++ pacman.js
@@ -1,4 +1,21 @@
 //board
 let board;
-let height;
-let width;
+const rowCount = 21 ;
+const columnCount =19;
+const tileSize = 32;
+const boardWidth = columnCount * tileSize;
+const boardHeight = rowCount * tileSize;
+let context;
+
+//sprites
+
+window.onload = function(){
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context = board.getContext("2d");
+}
+//getcontext('2d') - method returns a build-in canvasRenderingContext2D object .
+//provides methods and properties for drawing and manipulating graphics on the canvas.
+
+
